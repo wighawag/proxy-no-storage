5aba0d1c8baa24c751986e082fe2155b5daa7ab5
Wighawag <wighawag@gmail.com>
2020-09-16 11:23:02 +0100

    cleanup

--- src/OldProxyWithNoStorage.sol
+++ src/OldProxyWithNoStorage.sol
@@ -34,6 +34,38 @@ contract ProxyWithNoStorage {
             console.log("use implementation from storage: %s", implementationAddress);
         }
         
+        // address implementationAddress
+        // if (isContract(address(_implementationConfiguration))) {
+        //     implementationAddress = _implementationConfiguration.implementation();
+        //     console.log("use implementation from bytecode: %s", implementationAddress);
+        // } else {
+        //     implementationAddress = _implementationConfigurationRegistry.lastImplementation();
+        //     console.log("use implementation from storage: %s", implementationAddress);
+        // }
+
+        // The following fails because even with low level call solidity check if there is code
+        // address implementationAddress
+        // bytes memory returnData;
+        // bool s;
+        // (s, returnData) = address(_implementationConfiguration).call(abi.encodeWithSelector(_implementationConfiguration.implementation.selector));
+        // if (!s) {
+        //     console.log("impl: '%s'", string(returnData));
+        //     (s, returnData) = address(_implementationConfigurationRegistry).call(abi.encodeWithSelector(_implementationConfigurationRegistry.lastImplementation.selector));
+        //     require(s, string(returnData)); // TODO raw returnData   
+        // }
+        // implementationAddress = abi.decode(returnData, (address));
+
+        // same here:
+        // address implementationAddress
+        // try _implementationConfiguration.implementation() returns(address implementation) {
+        //     console.log("impl: '%s'", implementation);
+        //     implementationAddress = implementation;
+        // } catch{
+        //     // SO that it still work between the selfDestruct and upgrade
+        //     implementationAddress = _implementationConfigurationRegistry.lastImplementation();
+        //     console.log("noimpl: '%s'", implementationAddress);
+        // }
+
         // solhint-disable-next-line security/no-inline-assembly
         assembly {
             calldatacopy(0x0, 0x0, calldatasize())
@@ -53,7 +85,6 @@ contract ProxyWithNoStorage {
     function readImplementation() internal view returns(bool success, address implementation) {
         bytes4 input = _implementationConfiguration.implementation.selector;
         address implementationConfigurationAddress = address(_implementationConfiguration);
-        // solhint-disable-next-line security/no-inline-assembly
         assembly {
             let ptr := mload(0x40)
             mstore(ptr,input)
@@ -63,4 +94,16 @@ contract ProxyWithNoStorage {
         }
         success = success && implementation != address(0);
     }
+
+    // function isContract(address addr) internal view returns (bool) {
+    //     // for accounts without code, i.e. `keccak256('')`:
+    //     bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
+
+    //     bytes32 codehash;
+    //     // solium-disable-next-line security/no-inline-assembly
+    //     assembly {
+    //         codehash := extcodehash(addr)
+    //     }
+    //     return (codehash != 0x0 && codehash != accountHash);
+    // }
 }
